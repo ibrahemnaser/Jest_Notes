0d0485a4acc4ce3c703ee2ae030443c5874077e9
ibrahemnaser <i.naser4040@gmail.com>
2022-09-17 14:34:40 +0200

    dummy data

--- __test__/dummy.test.js
+++ __test__/dummy.test.js
@@ -6,22 +6,48 @@ const data = require("../dummy");
 // beforeEach(() => {
 //   console.log(56);
 // });
+describe.skip("First test suite", () => {
+  test("Check for the data lenght", () => {
+    expect(data.length).toBe(5);
+  });
+  test("Check for the data lenght", () => {
+    expect(data).toHaveLength(5);
+  });
+  test("Check for the first index type", () => {
+    expect(typeof data[0]).toBe("number");
+  });
+  test("Check for the second index type", () => {
+    expect(typeof data[1]).toBe("boolean");
+  });
+  test("Check for a specific number in arr", () => {
+    expect(data).toContain(false);
+  });
+  test("Check for a specific number not to be in arr", () => {
+    expect(data).not.toContain(true);
+  });
+});
 
-test("Check for the data lenght", () => {
-  expect(data.length).toBe(5);
-});
-test("Check for the data lenght", () => {
-  expect(data).toHaveLength(5);
-});
-test("Check for the first index type", () => {
-  expect(typeof data[0]).toBe("number");
-});
-test("Check for the second index type", () => {
-  expect(typeof data[1]).toBe("boolean");
-});
-test("Check for a specific number in arr", () => {
-  expect(data).toContain(false);
-});
-test("Check for a specific number not to be in arr", () => {
-  expect(data).not.toContain(true);
+describe("Test suite two of matchers", () => {
+  test("Check undefined", () => {
+    let a;
+    expect(a).toBeUndefined();
+  });
+  test("Check elzero RegExp", () => {
+    let name = "Elzero";
+    expect(name).toMatch(/Elzero/);
+  });
+  test("Check obj property with key only", () => {
+    let obj = {
+      name: "hello",
+      age: 55,
+    };
+    expect(obj).toHaveProperty("age");
+  });
+  test("Check obj property with key and value", () => {
+    let obj = {
+      name: "hello",
+      age: 55,
+    };
+    expect(obj).toHaveProperty("age", 55);
+  });
 });
